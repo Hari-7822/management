a0684fde1f54ab6e7afe90c6adab3b33f94a0578
Hari-7822 <h26058549@gmail.com>
2025-01-10 22:18:57 +0530

    tags

--- students/templatetags/tags.py
+++ students/templatetags/tags.py
@@ -1,8 +1,15 @@
+from datetime import datetime
+
 from django import template     
-from ..models import user 
+
+from ..models import user, student
 
 register = template.Library() 
   
 @register.simple_tag
 def Count(): 
       return user.objects.count()
+
+@register.simple_tag
+def InTime(request):
+      return datetime.now() - request.user.last_login
