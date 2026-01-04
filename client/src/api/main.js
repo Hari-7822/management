a5d812daa65a68747cbe4d5f5fd717e3a472489e
Hari-7822 <h26058549@gmail.com>
2026-01-05 02:34:35 +0530

    env_fix

--- client/src/api/main.js
+++ client/src/api/main.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.29.240:8000/api/',
+  baseURL: '',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
