eb82d0fa5e3cd91bc5176cb2da7c763abfb07997
Hari-7822 <h26058549@gmail.com>
2026-01-07 03:42:28 +0530

    utils

--- Server/utils/contrib/sql.py
+++ Server/utils/contrib/sql.py
@@ -1,13 +1,35 @@
-from django.forms import forms
+from django import forms
 from django.contrib.auth.forms import PasswordChangeForm
+from django.utils.functional import lazy
+from django.apps import apps 
 
-models = [
-    ("user", "User"),
-    ("student", "Student"), 
+def check(obj:dict, field_name):
+    try:
+        if ["name", "type", "null", "blank", "default", "unique", "db_index", "editable"] in obj.keys():
+            return True
+    except:
+        return False
+
+def get_model():
+    return [(
+        f"{model._meta.app_label}.{model.__name__}",
+        f"{model._meta.app_label} › {model.__name__}"
+        )for model in apps.get_models()
     ]
-class AddFieldForm(forms.Form):
-    model = forms.CharField(max_length=100, choices= Models,required=True)
-    additional_field = forms.JsonField(required=True)
 
-    def save():
-        model.add_field(**additional_field)
+models = lazy(get_model())
+
+class AddFieldForm(forms.Form):
+    model = forms.ChoiceField(choices= models,required=True)
+    field = forms.JSONField(required=True)
+    config = forms.JSONField(required=False)
+
+    def is_valid():
+
+        valid = super(AddFieldForm, self).is_valid()
+        if not valid:
+            return False
+        return True
+
+    def save(self):
+        self.model.add_field(*self.field, **self.config)
