23bfa67f41c6eae44981dd0b575955c841fd8712
Hari-7822 <h26058549@gmail.com>
2026-01-09 03:09:42 +0530

    db_fix

--- Server/API/views.py
+++ Server/API/views.py
@@ -143,4 +143,3 @@ class UserCreateRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-
